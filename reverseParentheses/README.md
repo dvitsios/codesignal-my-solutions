90fae1001720dccf1d207f695f19ddf16ba41a35
Dimitrios Vitsios <dvitsios@gmail.com>
2017-07-02 23:57:23 +0100

    added reverseParentheses solution

---
+++ reverseParentheses/README.md
@@ -0,0 +1,8 @@
+You have a string s that consists of English letters, punctuation marks, whitespace characters, and brackets. It is guaranteed that the parentheses in s form a regular bracket sequence.
+
+Your task is to reverse the strings contained in each pair of matching parentheses, starting from the innermost pair. The results string should not contain any parentheses.
+
+**Example**
+
+For string `s = "a(bc)de"`, the output should be
+`reverseParentheses(s) = "acbde"`.
---
+++ reverseParentheses/reverseParentheses.py
@@ -0,0 +1,32 @@
+def reverseParentheses(s):
+
+    bracket_pairs = []
+    
+    left_brackets_stack = []
+    
+    
+    for i in range(len(s)):
+        char = s[i]
+        if char == '(':
+            left_brackets_stack += [i]
+        elif char == ')':
+            left_bracket = left_brackets_stack[-1]
+            t = (left_bracket, i)
+            del left_brackets_stack[-1]
+
+            bracket_pairs += [(left_bracket, i)]
+             
+    print(bracket_pairs)
+            
+    for b in bracket_pairs:
+        left, right = b
+        
+        subs = s[left:right]
+        subs =  subs[::-1]
+        s = s[:left]+subs+s[right:]
+        print(s)
+    
+    s = s.replace(')', '')
+    s = s.replace('(', '')
+    return(s)
+
