09a0c3e8a3aa1535f232fe64d68faa546023bc45
Dimitrios Vitsios <dvitsios@gmail.com>
2017-07-04 23:01:59 +0100

    added incrementCipher challenge solution

--- incrementCipher/incrementCipher.py
+++ incrementCipher/incrementCipher.py
@@ -1,23 +1,22 @@
 def incrementCipher(s, nums):
-        
+
     for ni in nums:
-        
-        cnt = 1        
+
+        cnt = 1
         index = ni * cnt - 1
-        
+
         while index < len(s):
-                
+
                 incr_idx = ord(s[index]) + 1
                 if incr_idx == 123:
                         incr_idx = 97
-                new_ch = chr(incr_idx)
-                
-                s = s[:index] + new_ch + s[index+1:]
-                
+
+                s = s[:index] + chr(incr_idx) + s[index+1:]
+
                 cnt += 1
                 index = ni * cnt - 1
-                
+
     return s
-    
-                
-        
+
+
+
